14d09a62c5f6c843d8d678508287a801f3bdbf44
Tiệm Nhà Miu Miu <minht5475@gmail.com>
2026-09-14 15:23:01 +0700

    Update script.js

--- codetruyen/script.js
+++ codetruyen/script.js
@@ -121,7 +121,7 @@ const DAILY_PASSES = {
 ================================================== */
 
 const FACEBOOK_URL =
-    "https://www.facebook.com/profile.php?id=61594034743775";
+    "https://www.facebook.com/profile.php?id=6159403474377";
 
 
 /* ==================================================
