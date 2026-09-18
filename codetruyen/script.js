86a82f17bcb69f3be6b3dd205cc4cba46473bcb5
Tiệm Nhà Miu Miu <minht5475@gmail.com>
2026-09-19 04:18:56 +0700

    Update script.js

--- codetruyen/script.js
+++ codetruyen/script.js
@@ -8,7 +8,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
     const DAILY_PASSES = {
 
-        "2026-09-17": "san",
+        "2026-09-19": "be",
 
         "2026-09-18": "2026"
 
@@ -21,8 +21,8 @@ window.addEventListener("DOMContentLoaded", function () {
 
     const DAILY_FACEBOOK_POSTS = {
 
-        "2026-09-17":
-            "https://www.facebook.com/photo?fbid=122108962329467824&set=pcb.122108960919467824",
+        "2026-09-19":
+            "https://www.facebook.com/photo/?fbid=122110138095467824&set=pcb.122110138395467824&locale=vi_VN",
 
         "2026-09-18":
             "https://www.facebook.com/photo?fbid=122109445071467824&set=pcb.122109442095467824"
